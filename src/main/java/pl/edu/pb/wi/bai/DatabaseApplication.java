c21bef8bb4b68701fff5cc30a006dbd5189f2a54
Dominik <kruk.dominik.adrian@gmail.com>
2018-03-03 11:37:36 +0100

    Initial Comit

---
+++ src/main/java/pl/edu/pb/wi/bai/DatabaseApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pb.wi.bai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseApplication.class, args);
+
+	}
+}
